7e8cdd9dd4e21992e67ced112f25c0412c44c778
Rizki Cahya <rizki.cahya03@gmail.com>
2026-05-10 18:14:58 +0700

    feat: modify animes card & setup bootstrap icons

--- src/components/AnimeFav.tsx
+++ src/components/AnimeFav.tsx
@@ -9,31 +9,37 @@ export default function AnimeFav() {
             name: "Demon Slayer: Kimetsu no Yaiba",
             image: "/images/kny.jpg",
             genre: ["Action", "Historical", "Shounen"],
+            rating: "9.2",
         },
         {
             name: "Kono Subarashii Sekai ni Shukufuku wo!",
             image: "/images/konosuba.jpg",
             genre: ["Adventure", "Comedy", "Fantasy"],
+            rating: "9.2",
         },
         {
             name: "Haikyuu!!",
             image: "/images/haikyuu.jpg",
             genre: ["Sport", "Comedy", "Shounen"],
+            rating: "9.2",
         },
         {
             name: "Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka",
             image: "/images/danmachi.jpg",
             genre: ["Action", "Comedy", "Shounen"],
+            rating: "9.2",
         },
         {
             name: "Sword Art Online",
             image: "/images/sao.jpg",
             genre: ["Action", "Fantasy", "Shounen"],
+            rating: "9.2",
         },
         {
             name: "Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai",
             image: "/images/seishun-buta.jpg",
             genre: ["Drama", "Romance"],
+            rating: "9.2",
         },
     ];
 
@@ -43,20 +49,21 @@ export default function AnimeFav() {
                 <Row className="mb-3">
                     <h1>Anime Favorite</h1>
                 </Row>
-                <Row className="">
+                <Row className="d-flex justify-content-center">
                     <>
                         {animes.map((anime) => (
-                            <Col xs="6" sm="2" className="g-4">
+                            <Col xs="8" md="2" className="g-4">
                                 <Card className="shadow-sm h-100">
                                     <Card.Img variant="top" src={anime.image} alt={anime.name} />
                                     <Card.Body>
                                         <Card.Title className="card-title">{anime.name}</Card.Title>
                                         {anime.genre.map((genres) => (
-                                            <span className="pe-1" key={genres}>
+                                            <span className="badge text-bg-light me-1" key={genres}>
                                                 {genres}
                                             </span>
                                         ))}
                                     </Card.Body>
+                                    <Card.Footer className="card-footer"></Card.Footer>
                                 </Card>
                             </Col>
                         ))}
